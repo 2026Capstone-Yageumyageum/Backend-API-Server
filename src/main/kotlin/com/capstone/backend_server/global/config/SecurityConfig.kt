635f6977dc1137526526946493aa52dd1f90b11b
DDiyong236 <ydy236@gmail.com>
2026-04-26 18:42:13 +0900

    YA-27/feat(security) : Spring Security 및 REST API용 보안 기본 설정

---
+++ src/main/kotlin/com/capstone/backend_server/global/config/SecurityConfig.kt
@@ -0,0 +1,28 @@
+package com.capstone.backend_server.global.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf{ it.disable() }
+            .formLogin{ it.disable() }
+            .httpBasic{ it.disable() }
+
+            .authorizeHttpRequests { auth ->
+                auth.requestMatchers("/", "/login/**", "/oauth2/**", "/error").permitAll()
+                    .anyRequest().authenticated()
+            }
+            .oauth2Login { oauth2 ->
+
+            }
+        return http.build()
+    }
+}
