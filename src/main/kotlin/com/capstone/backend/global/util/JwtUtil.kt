23a79e4a118ec9fb8a10aa6c26a0b17feb9e4ce8
DDiyong236 <ydy236@gmail.com>
2026-04-27 16:19:55 +0900

    YA-61/feat(security) : jwt 토큰발급용 유틸리티 추가

---
+++ src/main/kotlin/com/capstone/backend/global/util/JwtUtil.kt
@@ -0,0 +1,53 @@
+package com.capstone.backend.global.util
+
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import java.util.Date
+import javax.crypto.SecretKey
+
+class JwtUtil(
+    @Value("\${jwt.secret}") private val secretString: String,
+    @Value("\${jwt.access-expiration}") private val accessExp: Long
+) {
+    private val secretKey: SecretKey = Keys.hmacShaKeyFor(secretString.toByteArray())
+
+    fun generateAccessToken(id: Long,email: String): String {
+        val now = Date()
+        val validity = Date(now.time + accessExp)
+
+        return Jwts.builder()
+            .claim("email",email)
+            .subject(id.toString())
+            .issuedAt(now)
+            .expiration(validity)
+            .signWith(secretKey)
+            .compact()
+    }
+    fun getIdFromToken(token: String): Long {
+        val subject = Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+            .subject
+
+        return subject.toLong()
+    }
+    fun getEmailFromToken(token: String): String? {
+        return Jwts.parser()
+            .verifyWith(secretKey)
+            .build()
+            .parseSignedClaims(token)
+            .payload
+            .get("email",String::class.java)
+    }
+    fun validateToken(token: String): Boolean {
+        return try{
+            Jwts.parser().verifyWith(secretKey).build().parseSignedClaims(token)
+            true
+        }catch(e: Exception){
+            false
+        }
+    }
+}
