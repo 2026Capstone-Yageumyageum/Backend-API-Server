62c0d05e92f1cbf923362980f1c695a8837cf640
DDiyong236 <ydy236@gmail.com>
2026-04-27 19:46:06 +0900

    YA-61/feat(auth): 리프레시 토큰 재발급 메서드 추가

--- src/main/kotlin/com/capstone/backend/domain/user/service/AuthService.kt
+++ src/main/kotlin/com/capstone/backend/domain/user/service/AuthService.kt
@@ -2,9 +2,13 @@ package com.capstone.backend.domain.user.service
 
 import com.capstone.backend.domain.user.dto.AuthResponse
 import com.capstone.backend.domain.user.dto.SignupRequest
+import com.capstone.backend.domain.user.dto.TokenResponse
+import com.capstone.backend.domain.user.entity.RefreshToken
 import com.capstone.backend.domain.user.entity.User
+import com.capstone.backend.domain.user.repository.RefreshTokenRepository
 import com.capstone.backend.domain.user.repository.UserRepository
 import com.capstone.backend.global.util.JwtUtil
+import com.google.api.client.auth.oauth2.RefreshTokenRequest
 import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier
 import com.google.api.client.http.javanet.NetHttpTransport
 import com.google.api.client.json.gson.GsonFactory
@@ -16,6 +20,7 @@ class AuthService(
     @Value("\${spring.security.oauth2.client.registration.google.client-id}")
     private val googleClientId: String,
     private val jwtUtil: JwtUtil,
+    private val refreshTokenRepository: RefreshTokenRepository
 ) {
     @Transactional
     fun verifyGoogleToken(idTokenString: String): AuthResponse {
@@ -55,5 +60,32 @@ class AuthService(
 
         val token = jwtUtil.generateAccessToken(savedUser.id!!, savedUser.email)
         return AuthResponse(request.email, true, "회원가입이 완료되었습니다.",token)
+    }
+    fun refreshTokens(requestToken: String): TokenResponse {
+        if(!jwtUtil.validateToken(requestToken)){
+            throw IllegalArgumentException("유효하지 않거나 만료된 리프레시 토큰입니다.")
+        }
+        val storedToken = refreshTokenRepository.findById(requestToken)
+            .orElseThrow{
+                IllegalArgumentException("이미 사용되었거나 만료된 토큰입니다. 다시 로그인해주세요.")
+            }
+        refreshTokenRepository.delete(storedToken)
+        val userID = storedToken.userId
+        val user = userRepository.findById(userID)
+            .orElseThrow {
+                IllegalArgumentException("사용자를 찾을 수 없습니다.")
+            }
+        val newAccessToken = jwtUtil.generateAccessToken(userID, user.email)
+        val newRefreshToken = jwtUtil.generateRefreshToken(userID)
+
+        refreshTokenRepository.save(
+            RefreshToken(
+                refreshToken = newRefreshToken,
+                userId = userID,
+                ttl = jwtUtil.refreshExp / 1000
+            )
+        )
+        return TokenResponse(newAccessToken, newRefreshToken)
+
     }
 }
