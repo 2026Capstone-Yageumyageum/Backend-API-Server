3f58cf32193345d1c0dbef753c9c22f48a52b939
DDiyong236 <ydy236@gmail.com>
2026-04-27 19:45:10 +0900

    YA-61/fix(auth): 자료형 선언 오류 수정

--- src/main/kotlin/com/capstone/backend/domain/user/repository/RefreshTokenRepository.kt
+++ src/main/kotlin/com/capstone/backend/domain/user/repository/RefreshTokenRepository.kt
@@ -3,4 +3,4 @@ package com.capstone.backend.domain.user.repository
 import com.capstone.backend.domain.user.entity.RefreshToken
 import org.springframework.data.repository.CrudRepository
 
-interface RefreshTokenRepository : CrudRepository<RefreshToken, Long>
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String>
